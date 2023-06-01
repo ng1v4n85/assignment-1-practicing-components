0c4712fb456d0363d2e602e52c46c312ba69ddd3
Ivan Backovic <ng1v4n85@gmail.com>
2023-06-01 12:59:29 +0200

    Solved all tasks

--- src/app/success-alert/success-alert.component.ts
+++ src/app/success-alert/success-alert.component.ts
@@ -3,7 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-success-alert',
   templateUrl: './success-alert.component.html',
-  styleUrls: ['./success-alert.component.css']
+  styles: [`
+    p {
+        background-color: palegreen;
+        border: 2px solid green;
+        padding: 2rem;
+    }
+  `]
 })
 export class SuccessAlertComponent {
 
